928bdcc6b342b30526fedaf0c24f700fbd998f0d
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-14 23:10:00 +0000

    display score

--- game.js
+++ game.js
@@ -21,6 +21,8 @@ let platforms;
 let player;
 let cursors;
 let stars;
+let score = 0;
+let scoreText;
 
 function preload() {
   this.load.image("sky", "assets/sky.png");
@@ -82,6 +84,11 @@ function create() {
 
   this.physics.add.collider(stars, platforms);
   this.physics.add.overlap(player, stars, collectStar, null, this);
+
+  scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    fill: "#000"
+  });
 }
 
 function update() {
@@ -102,4 +109,7 @@ function update() {
 
 function collectStar(player, star) {
   star.disableBody(true, true);
+
+  score += 10;
+  scoreText.setText(`Score: ${score}`);
 }
