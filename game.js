3b47f8129dc52eb6c9daad60451ca24db6c40cdc
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-14 21:58:14 +0000

    assets added and preloaded

--- game.js
+++ game.js
@@ -11,8 +11,20 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/ground.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48
+  });
+}
 
-function create() {}
+function create() {
+  this.add.image(400, 300, "sky");
+  this.add.image(400, 300, "star");
+}
 
 function update() {}
