a082d995f9eba3874340d8694fd25a6df9cb8d61
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-14 22:23:37 +0000

    physics enabled, platforms added, fix preload platform path

--- game.js
+++ game.js
@@ -2,6 +2,13 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: true
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -10,10 +17,11 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let platforms;
 
 function preload() {
   this.load.image("sky", "assets/sky.png");
-  this.load.image("ground", "assets/ground.png");
+  this.load.image("ground", "assets/platform.png");
   this.load.image("star", "assets/star.png");
   this.load.image("bomb", "assets/bomb.png");
   this.load.spritesheet("dude", "assets/dude.png", {
@@ -24,7 +32,15 @@ function preload() {
 
 function create() {
   this.add.image(400, 300, "sky");
-  this.add.image(400, 300, "star");
+
+  platforms = this.physics.add.staticGroup();
+  platforms
+    .create(400, 568, "ground")
+    .setScale(2)
+    .refreshBody();
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
 }
 
 function update() {}
